d8bf8c8fb2c27b37c1165c5dcd6577291c169b7a
SimonTheCoder <shihangyu@gmail.com>
2023-03-24 23:59:07 +0800

    now the bit can be filped by clicking.

--- number_board.py
+++ number_board.py
@@ -5,16 +5,37 @@ class NumberBoard:
         self.root = tk.Tk()
         self.root.title("Number Board")
         self.canvas = tk.Canvas(self.root, width=1024, height=50)
+        self.canvas.bind("<Button-1>", lambda event: self.handle_click(event))
         self.canvas.pack()
 
-        self.binary_label = tk.Label(self.root, text="Binary:", font=("Arial", 8))
+        self.frame1 = tk.Frame(self.root)
+        
+        self.binary_label = tk.Label(self.frame1, text="Binary:", font=("Arial", 8))
         self.binary_label.pack()
-        self.hex_label = tk.Label(self.root, text="Hexadecimal:", font=("Arial", 8))
+        self.hex_label = tk.Label(self.frame1, text="Hexadecimal:", font=("Arial", 8))
         self.hex_label.pack()
-        self.decimal_label = tk.Label(self.root, text="Decimal:", font=("Arial", 8))
+        self.decimal_label = tk.Label(self.frame1, text="Decimal:", font=("Arial", 8))
         self.decimal_label.pack()
-        self.octal_label = tk.Label(self.root, text="Octal:", font=("Arial", 8))
+        self.octal_label = tk.Label(self.frame1, text="Octal:", font=("Arial", 8))
         self.octal_label.pack()
+        
+        self.frame1.pack(side=tk.LEFT, padx=10)
+        self.frame2 = tk.Frame(self.root)
+
+        self.size_label = tk.Label(self.frame2, text="Size:", font=("Arial", 8))
+        self.size_label.pack()
+        self.kb_label = tk.Label(self.frame2, text="KB:", font=("Arial", 8))
+        self.kb_label.pack()
+        self.mb_label = tk.Label(self.frame2, text="MB:", font=("Arial", 8))
+        self.mb_label.pack()
+        self.gb_label = tk.Label(self.frame2, text="GB:", font=("Arial", 8))
+        self.gb_label.pack()
+        self.tb_label = tk.Label(self.frame2, text="TB:", font=("Arial", 8))
+        self.tb_label.pack()
+        self.frame2.pack(side=tk.LEFT, padx=10)
+
+
+
 
         
         self.textbox = tk.Entry(self.root)
@@ -50,7 +71,7 @@ class NumberBoard:
         for i in range(64):
             x = i * 16 + 10
             y = 10
-            self.canvas.create_text(x, y, text=str(64-i), font=("Arial", 8))
+            self.canvas.create_text(x, y, text=str(63-i), font=("Arial", 8))
             if bits[i] == 1:
                 self.canvas.create_rectangle(x - 5, y + 5, x + 5, y + 15, fill="black")
             else:
@@ -60,6 +81,36 @@ class NumberBoard:
         self.decimal_label.config(text="Decimal: {}".format(number))
         self.octal_label.config(text="Octal: {}".format(oct(number)[2:]))
 
+        self.size_label.config(text="Size:")
+        self.kb_label.config(text="KB: {}".format(number/1024))
+        self.mb_label.config(text="MB: {}".format(number/1024**2))
+        self.gb_label.config(text="GB: {}".format(number/1024**3))
+        self.tb_label.config(text="TB: {}".format(number/1024**4))
+
+        
+
+    def flip_bit(self, bit_index):
+        mask = 1 << bit_index
+        text = self.textbox.get()
+        if text.startswith("0x"):
+            number = int(text, 16)
+        else:
+            number = int(text)
+        number ^= mask
+        
+        self.textbox.delete(0, tk.END)
+        self.textbox.insert(0, hex(number))
+        self.draw_board()
+
+    def handle_click(self, event):
+        x = event.x
+        y = event.y
+        for i in range(64):
+            rect_x = i * 16 + 5
+            rect_y = 5
+            if rect_x <= x <= rect_x + 10 and rect_y <= y <= rect_y + 10 + 5 + 15:
+                self.flip_bit(63-i)
+                break
         
 
         
@@ -74,3 +125,4 @@ if __name__ == "__main__":
         nb = NumberBoard(sys.argv[1])
     else:
         nb = NumberBoard()
+
